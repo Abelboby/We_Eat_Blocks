535643e96d5e1a39afe7b3bd1e8229b5fc06533a
Adwaith Jayasankar <kichu31084@gmail.com>
2025-03-07 23:30:02 +0530

    Add Firebase configuration and service initialization

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// Import Firebase service - this will initialize Firebase on app start
+import './services/firebase_service.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
