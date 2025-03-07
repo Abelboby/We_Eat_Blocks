f326babd05a6456b201fb5c487260114423287e2
parthavpovil <parthavpovil@gmail.com>
2025-03-07 12:10:06 +0530

    Add  structs

--- companyFactory.sol
+++ companyFactory.sol
@@ -2,8 +2,29 @@
 pragma solidity ^0.8.20;
 
 import {Company} from "./Company.sol";
+import "./Ownable.sol";
 
 contract CompanyFactory {
-    address public owner;
-    mapping(address => Company) public companies;
-}
+     struct RegistrationRequest {
+        string companyName;
+        string companyType;
+        string registrationNumber;
+        string country;
+        string city;
+        string contactEmail;
+        string contactNumber;
+        address applicant;
+        bool approved;
+    }
+
+    struct ApprovedCompany {
+        string companyName;
+        string companyType;
+        string registrationNumber;
+        string country;
+        string city;
+        string contactEmail;
+        string contactNumber;
+        address companyAddress;
+        address companyContract;
+    }
